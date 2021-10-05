c6039edd396ae93284147436c1cbc6f09003c33d
Peyton Capers <peyton.capers27@gmail.com>
2021-10-05 18:43:41 -0500

    Added content and styling following instructor code along, implemented props

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Subcontents from './Subcontents'
 
 const Main = () => {
     return (
         <div className='main'>
-            Main
+            <Subcontents contentText={'Underwater Basket Weaving Class'} numberOfStudents={10} />
+            <Subcontents contentText={'Skydiving Class'} numberOfStudents={15} />
+            <Subcontents contentText={'Unicorn Riding Class'} numberOfStudents={300} />
         </div>
     )
 }
--- src/components/Subcontents.js
+++ src/components/Subcontents.js
@@ -1,9 +1,17 @@
 import React from 'react';
+//props is an object
+//keys and values
+//key is the 'attribute name'
+//value is fiven in the {} from the parent component
 
-const Subcontents = () => {
+
+const Subcontents = (props) => {
+    const { contentText, numberOfStudents } = props;
+
     return (
         <div className='subcontents'>
-            Subcontents
+            <h3>{contentText}</h3>
+            <p>Number of Students: {numberOfStudents}</p>
         </div>
     )
 }
